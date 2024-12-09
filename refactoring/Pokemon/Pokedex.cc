c76637cded05ce0c067dff35ef78460013215d97
Yorisoft <YelsinSepulveda@gmail.com>
2024-12-08 23:33:42 -0500

    Added more boiler plate code for Pokedex class
    Continue with guide on AppState management. Having to read more of it, from the start basically to get a full understanding. Might endup removing some of this code to the PokemonActivity class
    
    Test results: Passing, [  PASSED  ] 29 tests.

--- refactoring/Pokemon/Pokedex.cc
+++ refactoring/Pokemon/Pokedex.cc
@@ -1,3 +1,9 @@
+// Window Settings
+#define WINDOW_HEIGHT 480                   // window height in pixels
+#define WINDOW_WIDTH 640                    // window width in pixels
+#define DEPTH 16                            // window depth in pixels
+#define MAX_VISIBLE_ITEMS 5                 // Set the maximum number of visible
+
 #include<iostream>
 #include<vector>
 #include<string>
@@ -8,19 +14,164 @@
 #include"SQLstatements.h"
 
 Pokedex::Pokedex() {
+    window = NULL;
+    screen = NULL;
+    texture = NULL;
+    renderer = NULL;
+    font = NULL;
+
+
+    running = true;
 }
 
-int Pokedex::OnExecute() {
-	return 0;
+int Pokedex::onExecute() {
+    if (onInit() == false) {
+        return -1;
+    }
+
+    SDL_Event event;
+
+    while (running) {
+        while (SDL_PollEvent(&event)) {
+            onEvent(&event);
+        }
+
+        onLoop();
+        onRender();
+    }
+
+    onCleanup(); 
+
+    return 0;
+}
+
+bool Pokedex::onInit() {
+    if (SDL_Init(SDL_INIT_VIDEO | SDL_INIT_AUDIO) < 0) {
+        std::cout << "SDL could not initialize! SDL Error: " << SDL_GetError() << std::endl;
+        exit(EXIT_FAILURE);
+    }
+
+    this->window = SDL_CreateWindow(
+        "Pokedex",
+        SDL_WINDOWPOS_UNDEFINED,
+        SDL_WINDOWPOS_UNDEFINED,
+        WINDOW_WIDTH,
+        WINDOW_HEIGHT,
+        SDL_WINDOW_SHOWN
+    );
+    if (!this->window) {
+        std::cout << "Failed to create window: " << SDL_GetError();
+        SDL_Quit();
+        exit(EXIT_FAILURE);
+    }
+
+    this->renderer = SDL_CreateRenderer(
+        this->window,
+        -1,
+        SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC
+    );
+    if (!this->renderer) {
+        std::cout << "Failed to create renderer: " << SDL_GetError();
+        SDL_Quit();
+        exit(EXIT_FAILURE);
+    }
+
+    this->texture = SDL_CreateTexture(
+        this->renderer,
+        SDL_PIXELFORMAT_RGB565,
+        SDL_TEXTUREACCESS_STREAMING,
+        WINDOW_WIDTH,
+        WINDOW_HEIGHT
+    );
+    if (!this->texture) {
+        std::cout << "Failed to create texture: " << SDL_GetError();
+        SDL_Quit();
+        exit(EXIT_FAILURE);
+    }
+
+    this->screen = SDL_CreateRGBSurface(
+        0,
+        WINDOW_WIDTH,
+        WINDOW_HEIGHT,
+        DEPTH,
+        0, 0, 0, 0
+    );
+
+    if (TTF_Init() == -1) {
+        std::cout << "SDL could not initialize TTF_Init: " << TTF_GetError() << std::endl;
+        exit(EXIT_FAILURE);
+    }
+
+    this->font = TTF_OpenFont("res/font/Pokemon_GB.ttf", 20);
+    if (this->font == NULL) {
+        std::cout << "TTF_OpenFont: " << TTF_GetError() << std::endl;
+        exit(EXIT_FAILURE);
+    }
+
+    int imgFlags = IMG_INIT_PNG;
+    if (!(IMG_Init(imgFlags) & imgFlags)) {
+        std::cout << "SDL_image could not initialize! SDL_image Error: " << IMG_GetError();
+        exit(EXIT_FAILURE);
+    }
+
+    SDL_FillRect(
+        this->screen,
+        &(this->screen)->clip_rect,
+        SDL_MapRGB((this->screen)->format, 0x00, 0x00, 0x00)
+    );
+
+    /*if (Mix_OpenAudio(22050, MIX_DEFAULT_FORMAT, 2, 4096) == -1) {
+        printf("SDL_mixer could not initialize! SDL_mixer Error: %s\n", Mix_GetError());
+    }*/
+
+	return true;
+}
+
+void Pokedex::onEvent(SDL_Event* event) {
+    if (event->type == SDL_QUIT) {
+        running = false;
+    }
+}
+
+void Pokedex::onLoop() {
+}
+
+void Pokedex::onRender() {
+}
+
+void Pokedex::onCleanup() {
+    SDL_FreeSurface(screen);
+    SDL_DestroyTexture(texture);
+    SDL_DestroyRenderer(renderer);
+    SDL_DestroyWindow(window);
+    TTF_Quit();
+    IMG_Quit();
+    SDL_Quit();
+    //Mix_Quit();
 }
 
 int main(int argc, char* argv[]) {
-	std::string pokemonName= "charmander";
-	Pokemon* pokemon = new Pokemon(&pokemonName);
+/* 
+    BASIC GAME LOOP
+	Initialize();
 
-	// for testing.
-	std::cout << "This is pokemon object 'name' member variable: " << pokemon->getName() << std::endl;
-	std::cin.get();
+	while (true) {
+		Events();
+		Loop();
+		Render();
+	}
 
-	return 0;
+	Cleanup();
+*/
+	Pokedex pokedexApp;
+
+	return pokedexApp.onExecute();
+	//std::string pokemonName= "charmander";
+	//Pokemon* pokemon = new Pokemon(&pokemonName);
+
+	//// for testing.
+	//std::cout << "This is pokemon object 'name' member variable: " << pokemon->getName() << std::endl;
+	//std::cin.get();
+
+	//return 0;
 }
--- refactoring/Pokemon/Pokedex.h
+++ refactoring/Pokemon/Pokedex.h
@@ -2,15 +2,34 @@
 #define _POKEDEX_H_
 
 #include <SDL.h>
+#include <SDL_image.h>                   	
+#include <SDL_ttf.h>
 
 class Pokedex {
+private:
+	bool running;
+
+    SDL_Window* window = NULL;
+    SDL_Surface* screen = NULL;
+    SDL_Texture* texture = NULL;
+    SDL_Renderer* renderer = NULL;
+    TTF_Font* font = NULL;
 
 public:
 
     Pokedex();
 
-    int OnExecute();
+    int onExecute();
 
+    bool onInit();
+
+    void onEvent(SDL_Event* event);
+
+    void onLoop();
+
+    void onRender();
+
+    void onCleanup();
 };
 
 #endif
