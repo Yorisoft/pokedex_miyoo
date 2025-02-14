3dedaec08cbd2e73bfb34c15c48a5f87966124a4
Yorisoft <YelsinSepulveda@gmail.com>
2025-02-13 22:26:50 -0500

    remove trailing whitespace

--- Source/union-miyoomini-toolchain/workspace/retrodex/core/include/Pokedex.h
+++ Source/union-miyoomini-toolchain/workspace/retrodex/core/include/Pokedex.h
@@ -2,7 +2,7 @@
 #define _POKEDEX_H_
 
 #include <SDL2/SDL.h>
-#include <SDL2/SDL_image.h>                   	
+#include <SDL2/SDL_image.h>
 #include <SDL2/SDL_ttf.h>
 #include <SDL2/SDL_mixer.h>
 #include "PokedexActivityManager.h"
