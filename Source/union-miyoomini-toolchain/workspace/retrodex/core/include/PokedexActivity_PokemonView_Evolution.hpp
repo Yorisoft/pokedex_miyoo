1a3721245fc79c357d2fd32740d1b816cdced158
Yorisoft <YelsinSepulveda@gmail.com>
2025-03-23 20:15:31 -0400

    Renamed .h -> .hpp
    Provides clarity for tools like cloc and scc

--- Source/union-miyoomini-toolchain/workspace/retrodex/core/include/PokedexActivity_PokemonView_Evolution.hpp
+++ Source/union-miyoomini-toolchain/workspace/retrodex/core/include/PokedexActivity_PokemonView_Evolution.hpp
@@ -1,9 +1,9 @@
 #ifndef __POKEDEXACTIVITY_POKEMONVIEW_EVOLUTION__
 #define __POKEDEXACTIVITY_POKEMONVIEW_EVOLUTION__
 
-#include "AssetManager.h"
-#include "PokedexActivity.h"
-#include "Pokemon.h"
+#include "AssetManager.hpp"
+#include "PokedexActivity.hpp"
+#include "Pokemon.hpp"
 
 class PokedexActivity_PokemonView_Evolution : public PokedexActivity
 {
